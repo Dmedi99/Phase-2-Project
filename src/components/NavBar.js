224556dc966cdefa717778dab9c12a5d33533ed4
David <davidmedina0907@gmail.com>
2023-10-08 12:40:19 -0700

    added classnames in the JSX

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,51 +1,49 @@
-
-
 import React from "react";
 import {NavLink} from "react-router-dom"
 import './NavBar.css'
 
 
-//styles to add for NavBar links
-const linkStyles ={
-    textDecoration: "none", 
-    color: "black", 
-    padding: "10px 20px", 
-    marginRight: "10px", 
-    borderRadius: "5px", 
+const linkStyles = {
+    textDecoration: "none",
+    color: "#333",
+    padding: "10px 20px",
+    marginRight: "10px",
+    borderRadius: "5px",
     border: "1px solid #ccc",
-}
-
-function NavBar(){
+    transition: "background-color 0.3s ease-in-out",
+  }
+  
+  function NavBar() {
     return (
-        <div class="navbar">
-            {/* Navigation links using NavLink with to and exact props */}
-            <NavLink
-            to="/"
-            exact
-            style={linkStyles}
-            activestyle={{background:"darkgrey"}}
-            >
-                Home
-            </NavLink>
-            <NavLink
-            to="/recipes"
-            exact 
-            style={linkStyles}
-            activestyle={{background:"darkgrey"}}
-            >
-                Recipes
-            </NavLink>
-            <NavLink
-            to="/recipeform"
-            exact
-            style={linkStyles}
-            activestyle={{background:"darkgrey"}}
-            >
-                Post a Recipe
-            </NavLink>
-        </div>
-    )
-
-}
-
-export default NavBar
+      <div className="navbar">
+        {/* Navigation links using NavLink with to and exact props */}
+        <NavLink
+          to="/"
+          exact
+          style={linkStyles}
+          activeStyle={{ backgroundColor: "darkgrey" }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/recipes"
+          exact
+          style={linkStyles}
+          activeStyle={{ backgroundColor: "darkgrey" }}
+        >
+          Recipes
+        </NavLink>
+        <NavLink
+          to="/recipeform"
+          exact
+          style={linkStyles}
+          activeStyle={{ backgroundColor: "darkgrey" }}
+        >
+          Post a Recipe
+        </NavLink>
+      </div>
+    );
+  }
+  
+  export default NavBar;
+  

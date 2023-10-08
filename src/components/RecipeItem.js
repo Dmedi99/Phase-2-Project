c46d26d1027458af42e3c4ce6a70c8b01da8810d
David <davidmedina0907@gmail.com>
2023-10-08 02:47:42 -0700

    updated RecipeItem component, component is responsible to make an organized list of it fetched from the data.json

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -8,7 +8,12 @@ function RecipeItem({recipe}){
     return(
         <li>
           <h2>{recipe.name}</h2>
-            <p>Ingredients: {recipe.ingredients}</p>
+      <p>Ingredients:</p>
+      <ul>
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
             <p>Cook Time: {recipe.cook_time}</p>
             <p>Prep Time: {recipe.prep_time}</p>
             <p>Dietary Info: {recipe.dietary_info.join(', ')}</p>  

730e1a6986348259bccb0fe8d0155bbf2d68fa83
David <davidmedina0907@gmail.com>
2023-10-08 03:04:14 -0700

    gives an explanation at the top of the code of what the compnent does and how it uses the .join method when returning the JSX

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,3 +1,13 @@
+/* 
+The RecipeItem component is responsible for iterating the data fetched from the 
+data.json. That data is passed down from the RecipeList component (hence recipe 
+  being an argument for the component) Most of the work is done in the return 
+  JSX of the component. It get the recipe name, ingredients, cook time, prep time 
+  and the dietary information. It uses the .join method meaning which takes in 1 argument 
+  (the separator that will be used to join the elements) in this case a comma 
+*/
+
+
 import React from "react";
 
 

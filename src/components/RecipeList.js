e4af837470318fc8d05e6e8ac21e3c41436e813a
David <davidmedina0907@gmail.com>
2023-10-08 04:54:09 -0700

    added cleaner comments for explanantion of code

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,23 +3,23 @@ import './RecipeList.css'
 import RecipeItem from "./RecipeItem"
 
 function RecipeList() {
-    //state is being set 
-    const [recipes, setRecipes] =useState([])
+    // State is being set to store the list of recipes
+    const [recipes, setRecipes] = useState([])
 
-    useEffect(() =>{
+    useEffect(() => {
+        // Make a GET request to fetch recipes from the server
         fetch('http://localhost:8000/recipes')
-        .then((r)=>r.json())
-        .then((recipes) => setRecipes(recipes))
+            .then((r) => r.json())
+            .then((recipes) => setRecipes(recipes))
     }, []) //empty dependeny array is needed to avoid reccurring GET requests to the server 
 
     return (
         <section>
             <h1 className="title">Featured recipes</h1>
             <ul>
-                {/*The RecipeItem component is being rendered while passed down the recipe prop and 
-                the keys for them since its being mapped */}
-                {recipes.map((recipe)=>(
-                    <RecipeItem key={recipe.id} recipe={recipe}/>
+                {/*Map on the recipes. For each recipe it will render RecipeItem and pass down recipe as a prop*/}
+                {recipes.map((recipe) => (
+                    <RecipeItem key={recipe.id} recipe={recipe} />
                 ))}
             </ul>
         </section>

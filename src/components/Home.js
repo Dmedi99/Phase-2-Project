71625c73056ea071291dc821a4a8e065c20721e8
David <davidmedina0907@gmail.com>
2023-10-08 12:38:41 -0700

    added classnames to the JSX elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,16 @@ import './Home.css'
 
 function Home(){
     return(
-        <div>
+        <div className="home-container">
             <h2 className="home-title">Welcome to 'What's Cookin' </h2>
-            <h3 className="description">
-                <p>
+            <h3 className="home-description">
                 The purpose of this site is to create a platform where people can share 
                 and explore recipes! Get ride of the indicisiveness you get trying to figure 
                 out what's for dinner. You'll be able to find something that'll catch your eye 
                 just seconds after scrolling. It's also a great resource to find delicious 
                 recipes for certain diets. Wether it's a side dish for the table or the perfect
-                midnight snack someone will have what you are looking for. Happy cooking! 
-                </p>
+                midnight snack someone will have what you are looking for. 
+                <h3>Happy cooking! </h3>
             </h3>
         </div>
     )

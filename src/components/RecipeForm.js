01a4a9c67ca2ab7206652057bbf87a86424db583
David <davidmedina0907@gmail.com>
2023-10-08 02:46:21 -0700

    updated recipe form component which now successfully makes a POST request to data.json when it is being ran

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,121 +1,111 @@
-import React, {useState} from "react";
-import './RecipeForm.css'
+import React, { useState } from "react";
+import './RecipeForm.css';
 
+function RecipeForm() {
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
 
-function RecipeForm(){
-    const [formData, setFormData] = useState({
-        name:'',
-        ingredients:'',
-        cook_time:'',
-        prep_time:'',
-        dietary_info:'',
-    })
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    const dietaryOptions = [
-        "Low Carb",
-        "High Protien",
-        "Keto",
-        "Paleo",
-    ]
-
-    function handleInputChange(e){
-        const {name, value} = e.target
-        setFormData({
-            ...formData,
-            [name]: value,
-        })
-    }
-
-    function handleSubmit(e){
-        e.preventDefault()
-        fetch('http://localhost:8000/recipes', {
-            method:'POST',
-            headers: {
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(formData),
-        })
-        .then((response)=>{
-            if (!response.ok){
-                throw new Error('response failed')
-            }
-            return response.json()
-        })
-        .then((newRecipe) =>{
-            console.log('New Recipe:', newRecipe)
-        })
-    }
-
-
-    return (
-        <div>
-        <h2>Add a New Recipe</h2>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Recipe Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="ingredients">Ingredients:</label>
-                <textarea
-                    id="ingredients"
-                    name="ingredients"
-                    value={formData.ingredients}
-                    onChange={handleInputChange}
-                    required
-                ></textarea>
-            </div>
-            <div>
-                <label htmlFor="cook_time">Cook Time:</label>
-                <input
-                    type="text"
-                    id="cook_time"
-                    name="cook_time"
-                    value={formData.cook_time}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="prep_time">Prep Time:</label>
-                <input
-                    type="text"
-                    id="prep_time"
-                    name="prep_time"
-                    value={formData.prep_time}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="dietary_info">Dietary Info:</label>
-                <select
-                    id="dietary_info"
-                    name="dietary_info"
-                    value={formData.dietary_info}
-                    onChange={handleInputChange}
-                    required
-                >
-                    <option value="" disabled>Select dietary info</option>
-                    {dietaryOptions.map((option) => (
-                        <option key={option} value={option}>
-                            {option}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-)
     
+    const newRecipe = {
+      name: formData.name,
+      ingredients: formData.ingredients.split("\n").map((ingredient) => ingredient.trim()),
+      cook_time: formData.cook_time,
+      prep_time: formData.prep_time,
+      dietary_info: formData.dietary_info.split(",").map((info) => info.trim()),
+    };
+
+    fetch("http://localhost:8000/recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newRecipe),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setFormData({
+            name: "",
+            ingredients: "",
+            cook_time: "",
+            prep_time: "",
+            dietary_info: "",
+          });
+        } else {
+          console.error("Failed to add a new recipe.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error posting data:", error);
+      });
+  }
+
+  const [formData, setFormData] = useState({
+    name: "",
+    ingredients: "",
+    cook_time: "",
+    prep_time: "",
+    dietary_info: "",
+  });
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Recipe Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Ingredients (One per line):
+          <textarea
+            name="ingredients"
+            value={formData.ingredients}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Cook Time:
+          <input
+            type="text"
+            name="cook_time"
+            value={formData.cook_time}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Prep Time:
+          <input
+            type="text"
+            name="prep_time"
+            value={formData.prep_time}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Dietary Info (Comma-separated):
+          <input
+            type="text"
+            name="dietary_info"
+            value={formData.dietary_info}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default RecipeForm
+export default RecipeForm;
